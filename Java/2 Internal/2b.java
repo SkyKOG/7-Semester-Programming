632233a7dba2162b54ccf90545b21f715ea595a6
Akash Devaraju <akashdevaraju@gmail.com>
2012-10-09 16:20:04 +0530

    Added Prog 2b java 2int

---
+++ Java/2 Internal/2b.java
@@ -0,0 +1,30 @@
+//Author : SKyKOG
+
+import java.sql.* ;
+
+class JDBCQuery
+{
+  public static void main( String args[] )
+  {
+     {
+      Class.forName( "sun.jdbc.odbc.JdbcOdbcDriver" ) ;
+      
+      Connection conn = DriverManager.getConnection( "jdbc:odbc:SDB" ) ;
+
+      Statement stmt = conn.createStatement() ;
+      
+      ResultSet rs = stmt.executeQuery( "SELECT empage FROM emp" ) ;
+
+      while( rs.next() )
+         System.out.println( rs.getString(1) ) ;
+
+      rs = stmt.executeQuery( "SELECT count(*) FROM emp where empage > 50" ) ;
+
+      while( rs.next() )
+         System.out.println( rs.getString(1) ) ;
+
+      rs.close() ;
+      stmt.close() ;
+      conn.close() ;
+  }
+}
