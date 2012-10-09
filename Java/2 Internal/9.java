6f60f7f6aadef43f5d98d4a1bfb5be12bc2b1bc1
Akash Devaraju <akashdevaraju@gmail.com>
2012-10-09 14:19:28 +0530

    Added Prog 9 for java 2nd int

---
+++ Java/2 Internal/9.java
@@ -0,0 +1,33 @@
+//Author : SkyKOG
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JTableDemo extends JApplet {
+	
+	public void init() {
+		SwingUtilities.invokeAndWait(
+			new Runnable() {
+				public void run() {
+					makeGUI();
+				}
+			}
+		);
+	}
+
+	private void makeGUI() {
+		String[] colHeads = { "fname", "lname", "age" };
+		
+		Object[][] data = {
+		{ "Sky", "KOG", "21" },
+		{ "Vish", "Pand", "21" },
+		{ "Shrivar", "Jois", "21" },
+		{ "Chris", "Markus", "21" },
+		{ "Sagar", "Dev", "15" },
+		};
+
+		JTable table = new JTable(data, colHeads);
+		JScrollPane jsp = new JScrollPane(table);
+		add(jsp);
+	}
+}
