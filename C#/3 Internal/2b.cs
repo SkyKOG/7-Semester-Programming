49e34146e730cca94d5738cb7ae207cf4c5fcd0c
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-06 19:32:17 +0530

    Added Prog 2b C# 3 Internal

---
+++ C#/3 Internal/2b.cs
@@ -0,0 +1,45 @@
+//Author : SkyKOG
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ComplexNumber c = new ComplexNumber(5, -6);
+        Console.WriteLine("Conversion to string: {0}", Convert.ToString(c));
+        Console.WriteLine("Conversion to double: {0}", Convert.ToDouble(c));
+    }
+}
+
+public struct ComplexNumber : IConvertible
+{
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public TypeCode GetTypeCode()
+    {
+        return TypeCode.Object;
+    }
+
+    public double ToDouble(IFormatProvider provider)
+    {
+        return this.real;
+    }
+
+    public string ToString(IFormatProvider provider)
+    {
+        return this.real.ToString() + "," this.imaginary.ToString() + "i";
+    }
+
+    public allOtherFormats ToallOtherFormats(IFormatProvider provider)
+    {
+        throw new InvalidCastException();
+    }
+}
