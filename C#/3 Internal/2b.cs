f9f521d9af7fa04b255191b8e0187e5aff0f394c
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-06 23:54:43 +0530

    Fixed spelling mistakes

--- C#/3 Internal/2b.cs
+++ C#/3 Internal/2b.cs
@@ -49,12 +49,12 @@ dynamic type converion in C++.
 In the above progam the ComplexNumber structure has been interfaced with IConvertible 
 (struct ComplexNumber : IConvertible) this allows the structure to recieve/return type information at runtime.
 
-The property IFormatProvider provider recieves the type information and depending weather its a string/double 
+The property "IFormatProvider provider" recieves the type information and depending weather its a string/double 
 carries out the respective funtion.
 
-For all other data types and exception is thrown. 
+For all other data types an exception is thrown. 
 
-In short the IConvertible interface allows the programmer to "convert" and entire object to one of the basic data
-tyoes or other types as the requirement may be.
+In short the IConvertible interface allows the programmer to "convert" an entire object to one of the basic data
+types or other types as the requirement may be.
 
 */
