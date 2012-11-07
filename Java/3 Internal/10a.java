d448fe6db64303a05fa681b6df9f89a7dcbc95c9
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-07 16:40:34 +0530

    Added Prog 10 Java 3 Internal

---
+++ Java/3 Internal/10a.java
@@ -0,0 +1,22 @@
+// Author : SkyKOG
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class postExample extends HttpServlet {
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response)
+	throws ServletException, IOException {
+		
+		String theName = request.getParameter("name");
+		
+		response.setContentType("text/html");
+		
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("<B>Have a Good Day : ");
+		pw.println(theName);
+		pw.close();
+	}
+}
